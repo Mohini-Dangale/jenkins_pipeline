2ff7eba678c9d8ab720ad60c96a4ece41e65e83b
unknown <mohini.dangale@gmail.com>
2020-01-20 21:57:26 +0530

    my changes

---
+++ src/main/java/com/md/training/jenkins/JenkinsCalculator.java
@@ -0,0 +1,16 @@
+package com.md.training.jenkins;
+
+public class JenkinsCalculator {
+	
+	
+	//Method to add 2 numbers
+		public int addNumbers(int numberOne, int numberTwo) {
+			return numberOne + numberTwo;
+		}
+		
+		//Method 2 subtract 2 numbers
+		public int subtractNumbers(int numberOne, int numberTwo) {
+			return numberOne - numberTwo;
+		}
+
+}
